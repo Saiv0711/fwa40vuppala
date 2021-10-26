74b643cadf8b505e8f2ad349b86c769466e370ef
Saiv0711 <S545040@nwmissouri.edu>
2021-10-26 18:02:27 -0500

    updated

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata',{ title: 'Sai krishna Vuppala'});
+  res.render('mydata', { title: 'Sai krishna Vuppala'});
 });
 
 module.exports = router;
